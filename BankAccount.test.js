e0c6225c630e5fa8cb1bd484f8b0f21b2b72f161
Lydia Ntafa <lydiadafa@gmail.com>
2023-09-05 14:17:44 +0100

    tested to deposit float number

--- BankAccount.test.js
+++ BankAccount.test.js
@@ -6,9 +6,14 @@ describe('Bank Account', () => {
         expect(Account.printStatement()).toStrictEqual("date || credit || debit || balance")
     });
 
-    it('allows the user to make a deposit', () => {
+    it('allows the user to make a deposit of 1000', () => {
         const Account = new BankAccount();
         expect(Account.deposit(1000)).toStrictEqual("Transaction complete. New account balance: 1000.00")
+    });
+
+    it('allows the user to make a deposit using a float', () => {
+        const Account = new BankAccount();
+        expect(Account.deposit(0.5)).toStrictEqual("Transaction complete. New account balance: 0.50")
     });
 });
 
