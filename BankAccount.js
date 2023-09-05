c620f97967632042c994c56ca2b87697ecd4dcac
Lydia Ntafa <lydiadafa@gmail.com>
2023-09-05 17:06:17 +0100

    added the 'transaction complete message' to the 'createTransaction' function

--- BankAccount.js
+++ BankAccount.js
@@ -1,37 +1,43 @@
 class BankAccount {
+
     #statement = [];
     #balance = 0;
     
-
     #balanceCalculation(amount) {
         this.#balance = amount + this.#balance;
     }
+
+    #createTransaction(transaction, amount) {
+        const currentDate = (new Date()).toLocaleDateString('en-GB');
+        var action = ""
+        if (transaction === "deposit") {
+            this.#balanceCalculation(amount);
+            action = `${currentDate} || || ${amount.toFixed(2)} || ${this.#balance.toFixed(2)}`;
+        } else {
+            this.#balanceCalculation(-amount);
+            action = `${currentDate} || ${amount.toFixed(2)} || || ${this.#balance.toFixed(2)}`;
+        }
+        this.#statement.push(action);
+        return `Transaction complete. New account balance: ${this.#balance.toFixed(2)}`
+    }
     
     printStatement() {
+        const header = "date || credit || debit || balance"
         if (this.#statement.length === 0){
-            return "date || credit || debit || balance"
+            return header
         }
-        return `date || credit || debit || balance\n`+`${this.#statement}`;
+        return header+`\n`+`${this.#statement}`;
     }
 
     deposit(amount) {
-        this.#balanceCalculation(amount);
-        var currentDate = (new Date()).toLocaleDateString('en-GB');
-        const action = `${currentDate} || || ${amount.toFixed(2)} || ${this.#balance.toFixed(2)}`;
-        this.#statement.push(action);
-        return `Transaction complete. New account balance: ${this.#balance.toFixed(2)}`
+        return this.#createTransaction("deposit", amount);
     }
 
     withdrawal(amount){
         if (amount > this.#balance) {
             return `Cannot complete this transaction due to insufficient balance. Account balance: ${this.#balance.toFixed(2)}`
         }
-        this.#balanceCalculation(-amount);
-        var currentDate = (new Date()).toLocaleDateString('en-GB');
-        const action = `${currentDate} || ${amount.toFixed(2)} || || ${this.#balance.toFixed(2)}`;
-        this.#statement.push(action);
-        return `Transaction complete. New account balance: ${this.#balance.toFixed(2)}`
-        
+        return this.#createTransaction('withdrawal', amount);
     }
 
 };
