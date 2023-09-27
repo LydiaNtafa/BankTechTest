5ff837d1afc983ba692254f766c046755e60c2a6
Lydia Ntafa <you@example.comlydiadafa@gmail.com>
2023-09-27 14:17:40 +0100

    initiating the new transaction class and unit tests

--- Transaction.js
+++ Transaction.js
@@ -1,5 +1,11 @@
 class Transaction {
-    
+    constructor() {
+        this.date = this.#todaysDateAsString();
+    }
+
+    #todaysDateAsString() {
+        return (new Date()).toLocaleDateString('en-GB');
+    }
 }
 
 module.exports = Transaction;
--- Transaction.test.js
+++ Transaction.test.js
@@ -1,7 +1,21 @@
-const Transaction = require("./Transaction")
+const Transaction = require("./Transaction");
+const originalDate = Date;
 
 describe('Transaction', () => {
-    it("creates a new transaction that has type:deposit and amount:1000")
-    const transaction = new Transaction("deposit", 1000)
-    // expect Transaction.
+    it("creates a transaction object that has a date" , () => {
+        global.Date = class extends Date {
+            constructor() {
+                super('1993-09-22');
+            }
+        };
+        const transaction = new Transaction("deposit", 1000, 40000);
+        expect(transaction.date).toStrictEqual(`22/09/1993`);
+        global.Date = originalDate;
+    })
+
+    // it("returns the transaction in the required format date || credit || debit || balance", () => {
+    //     const transaction = new Transaction("22/09/1993", "deposit", 1000, 40000);
+    //     expect(transaction.format()).toStrictEqual(`22/09/1993 || 1000.00 || || 4000.00`);
+    // })
+    
 })
