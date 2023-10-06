46b1dbc7fdc3e6c79cee966c6f8b0e313dcee7a9
Lydia Ntafa <you@example.comlydiadafa@gmail.com>
2023-10-06 19:55:04 +0100

    transaction class tested for constuctor and format() for deposit

--- Transaction.js
+++ Transaction.js
@@ -1,10 +1,16 @@
 class Transaction {
-    constructor() {
+    constructor(transactionType, amount, newBalance) {
         this.date = this.#todaysDateAsString();
+        this.amount = amount;
+        this.newBalance = newBalance;
     }
 
     #todaysDateAsString() {
         return (new Date()).toLocaleDateString('en-GB');
+    }
+
+    format() {
+        return `${this.date} || ${this.amount.toFixed(2)} || || ${this.newBalance.toFixed(2)}`
     }
 }
 
--- Transaction.test.js
+++ Transaction.test.js
@@ -8,14 +8,20 @@ describe('Transaction', () => {
                 super('1993-09-22');
             }
         };
-        const transaction = new Transaction("deposit", 1000, 40000);
+        const transaction = new Transaction("deposit", 1000, 4000);
         expect(transaction.date).toStrictEqual(`22/09/1993`);
         global.Date = originalDate;
     })
 
-    // it("returns the transaction in the required format date || credit || debit || balance", () => {
-    //     const transaction = new Transaction("22/09/1993", "deposit", 1000, 40000);
-    //     expect(transaction.format()).toStrictEqual(`22/09/1993 || 1000.00 || || 4000.00`);
-    // })
+    it("returns the transaction in the required format date || credit || debit || balance", () => {
+        global.Date = class extends Date {
+            constructor() {
+                super('1993-09-22');
+            }
+        };
+        const transaction = new Transaction("deposit", 1000, 4000);
+        expect(transaction.format()).toStrictEqual(`22/09/1993 || 1000.00 || || 4000.00`);
+        global.Date = originalDate;
+    })
     
 })
