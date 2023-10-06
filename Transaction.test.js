d9e4e2507043e80f76086e750d7fa87569f974a0
Lydia Ntafa <you@example.comlydiadafa@gmail.com>
2023-10-06 20:22:55 +0100

    format() tested for deposit and withdraw

--- Transaction.test.js
+++ Transaction.test.js
@@ -13,7 +13,7 @@ describe('Transaction', () => {
         global.Date = originalDate;
     })
 
-    it("returns the transaction in the required format date || credit || debit || balance", () => {
+    it("returns the deposit transaction in the required format date || credit || debit || balance", () => {
         global.Date = class extends Date {
             constructor() {
                 super('1993-09-22');
@@ -21,6 +21,17 @@ describe('Transaction', () => {
         };
         const transaction = new Transaction("deposit", 1000, 4000);
         expect(transaction.format()).toStrictEqual(`22/09/1993 || 1000.00 || || 4000.00`);
+        global.Date = originalDate;
+    })
+
+    it("returns the withdraw transaction in the required format date || credit || debit || balance", () => {
+        global.Date = class extends Date {
+            constructor() {
+                super('2000-01-01');
+            }
+        };
+        const transaction = new Transaction("withdraw", 666.6, 333.4);
+        expect(transaction.format()).toStrictEqual(`01/01/2000 || || 666.60 || 333.40`);
         global.Date = originalDate;
     })
     
